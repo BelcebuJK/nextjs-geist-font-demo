b76922dff598995b1aa9d44f67e09a1d7b918a14
Belcebú <86620518+BelcebuJK@users.noreply.github.com>
2025-08-11 20:49:18 -0600

    Add public/set-java-home.bat 1754966957257

---
+++ public/set-java-home.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Script para configurar JAVA_HOME en Windows
+
+SET JAVA_HOME="C:\Program Files\Java\jdk-xx.x.x_xx"
+SET PATH=%JAVA_HOME%\bin;%PATH%
+
+echo JAVA_HOME configurado a %JAVA_HOME%
+pause
